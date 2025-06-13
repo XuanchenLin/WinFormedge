3551bb56ad3416c051ade3c74b6dc8e2239d7deb
林选臣 <xuanchenlin@msn.com>
2025-06-13 11:32:28 +0800

    🪳 Fixed an issue in WinFormedge.Blazor that could cause the initialization script to not load properly.

---
+++ src/WinFormedge.Blazor/AutoCloseStream.cs
@@ -0,0 +1,54 @@
+﻿// This file is part of the WinFormedge project.
+// Copyright (c) 2025 Xuanchen Lin all rights reserved.
+// This project is licensed under the MIT License.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormedge.Blazor;
+internal class AutoCloseStream : Stream
+{
+    private readonly Stream _baseStream;
+
+    public AutoCloseStream(Stream baseStream)
+    {
+        _baseStream = baseStream;
+    }
+
+    public override bool CanRead => _baseStream.CanRead;
+
+    public override bool CanSeek => _baseStream.CanSeek;
+
+    public override bool CanWrite => _baseStream.CanWrite;
+
+    public override long Length => _baseStream.Length;
+
+    public override long Position { get => _baseStream.Position; set => _baseStream.Position = value; }
+
+    public override void Flush() => _baseStream.Flush();
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        var bytesRead = _baseStream.Read(buffer, offset, count);
+
+        // Stream.Read only returns 0 when it has reached the end of stream
+        // and no further bytes are expected. Otherwise it blocks until
+        // one or more (and at most count) bytes can be read.
+        if (bytesRead == 0)
+        {
+            _baseStream.Close();
+        }
+
+        return bytesRead;
+    }
+
+    public override long Seek(long offset, SeekOrigin origin) => _baseStream.Seek(offset, origin);
+
+    public override void SetLength(long value) => _baseStream.SetLength(value);
+
+    public override void Write(byte[] buffer, int offset, int count) => _baseStream.Write(buffer, offset, count);
+}
--- src/WinFormedge.Blazor/BlazorHybridExtensions.cs
+++ src/WinFormedge.Blazor/BlazorHybridExtensions.cs
@@ -2,11 +2,29 @@
 
 public static class BlazorHybridExtensions
 {
+    /// <summary>
+    /// Sets the virtual host name for a Blazor Hybrid application on the Formedge instance.
+    /// </summary>
+    /// <param name="formedge">
+    /// The Formedge instance to configure.
+    /// </param>
+    /// <param name="options">
+    /// The options for the Blazor Hybrid application, including the root component type, host path, and other settings.
+    /// </param>
     public static void SetVirtualHostNameToBlazorHybrid(this Formedge formedge, BlazorHybridOptions options)
     {
         formedge.RegisterWebResourceHander(new BlazorHybridResourceHandler(options, formedge));
     }
 
+    /// <summary>
+    /// Clears the virtual host name for a Blazor Hybrid application on the Formedge instance.
+    /// </summary>
+    /// <param name="formedge">
+    /// The Formedge instance to configure. 
+    /// </param>
+    /// <param name="options">
+    /// The options for the Blazor Hybrid application, which should match the options used when setting the virtual host name.
+    /// </param>
     public static void ClearVirtualHostNameToBlazorHybrid(this Formedge formedge, BlazorHybridOptions options)
     {
         formedge.UnregisterWebResourceHandler(options.Scheme, options.HostName);
